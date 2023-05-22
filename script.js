0e626a7f7f19249fcf6b22aed1f987703750bee6
Aidan <survivalking4@gmail.com>
2023-05-21 18:10:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+/*
+config: {
+    hourlyWage: number
+    running: boolean
+    lastTap: timestamp
+    preStartTotal: number
+}
+*/
+
+config = {
+    hourlyWage: 15,
+    running: false,
+    lastTap: Date.now(),
+    preStartTotal: 0
+};
+if (localStorage.getItem("wagetrack_config")) {
+    config = JSON.parse(localStorage.getItem("wagetrack_config"))
+}
+
+function toggle() {
+    if (!config.running) {
+        config.running = true;
+        config.lastTap = Date.now();
+    } else {
+        config.preStartTotal = calculateHoursWorked();
+        config.running = false;
+        config.lastTap = Date.now();
+    }
+    saveConfig();
+}
+
+function saveConfig() {
+    localStorage.setItem("wagetrack_config", JSON.stringify(config));
+}
+
+function calculateHoursWorked() {
+    if (config.running) {
+        return (Date.now()-config.lastTap)/1000/60/60+config.preStartTotal;
+    } else {
+        return config.preStartTotal;
+    }
+}
+
+function calculatePayment() {
+    return calculateHoursWorked()*config.hourlyWage;
+}
+
+function updateTick() {
+    if (config.running) {
+        button.classList.remove("stopped");
+        button.classList.add("running");
+        button.innerText = "Stop";
+    } else {
+        button.classList.add("stopped");
+        button.classList.remove("running");
+        button.innerText = "Start";
+    }
+    dollarAmount.innerText = "$" + roundCents(calculatePayment());
+    wageAmount.innerText = "$" + roundCents(config.hourlyWage);
+}
+
+function roundCents(num) {
+    return (Math.floor(num*100)/100).toFixed(2);
+}
+
+function loop() {
+    requestAnimationFrame(loop);
+    updateTick();
+}
+
+window.onload = function() {
+    doStartupCheck();
+    loop();
+}
+
+function doStartupCheck() {
+    var oldDate = new Date(config.lastTap);
+    var newDate = new Date();
+    if (newDate.getDate() != oldDate.getDate()) {
+        reset();
+    }
+    saveConfig();
+}
+
+function changeWage() {
+    config.hourlyWage = +(prompt("Enter your new hourly pay:", roundCents(config.hourlyWage)));
+    saveConfig()
+}
+
+function reset() {
+    config.running = false;
+    config.lastTap = 0;
+    config.preStartTotal = 0;
+}
